1ab6d955b5e89123c2c0dcbfd7ffee73d3f93fb7
kye <kye@kyeui-iMac.local>
2026-01-06 22:07:55 +0900

    feaature: mockdata added

--- src/pages/dashboard/mockdata.ts
+++ src/pages/dashboard/mockdata.ts
@@ -5,8 +5,13 @@ export const pieData: RoomEnergy[] = [
 ];
 
 export const lineData: EnergyLineData[] = [
-  { time: "09:00", value: 120 },
+  { time: "08:00", value: 120 },
   { time: "10:00", value: 180 },
   { time: "11:00", value: 150 },
+  { time: "13:00", value: 220 },
+  { time: "14:00", value: 220 },
+  { time: "15:00", value: 220 },
+  { time: "16:00", value: 220 },
+  { time: "12:00", value: 220 },
   { time: "12:00", value: 220 },
 ];
